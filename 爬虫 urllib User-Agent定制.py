16434ce641d88d7bbb84a8ae9bcb5cf086e73520
kai <uch.python12@gmail.com>
2023-02-12 16:47:06 +0800

    Create 爬虫 urllib User-Agent定制.py

---
+++ 爬虫 urllib User-Agent定制.py
@@ -0,0 +1,19 @@
+from urllib import request
+
+# 使用urlib獲取url代碼
+
+# 定義一個url 訪問的地址
+url = ''
+
+headers = {
+    "User-Agent": ""}
+
+request_headers= request.Request(url=url,headers=headers)
+
+# 模擬瀏覽器向服務器發送請求 response 響應
+response = request.urlopen(request_headers)
+
+# 獲取響應頁面的原碼
+content = response.read().decode('utf-8')
+
+print(content)
